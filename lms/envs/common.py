62d9c9d40d24d479048dc2a85699dbb1fecd13a4
lokeshk-incresco <lokesh.kasi@increscotech.com>
2023-07-19 10:01:47 +0530

    Revert "config changes"
    This reverts commit 12c40e95eb409047f3bb1a3de1de5a82865a9885.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2979,7 +2979,7 @@ INSTALLED_APPS = [
     'django.contrib.redirects',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'chatgptxblock'
+    'chatgpt-xblock'
     # Tweaked version of django.contrib.staticfiles
     'openedx.core.djangoapps.staticfiles.apps.EdxPlatformStaticFilesConfig',
 
